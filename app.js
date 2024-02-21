ee98bfae45d3fcdb7cb97ac30623616de239952b
alexwbi88 <147102940+alexwbi88@users.noreply.github.com>
2024-02-21 15:40:12 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+const server = app.listen(port, () => console.log(`Example app in a PR listening on port ${port}!`));
 
 server.keepAliveTimeout = 130 * 1000;
 server.headersTimeout = 120 * 1000;
@@ -13,7 +13,7 @@ const html = `
 <!DOCTYPE html>
 <html>
   <head>
-    <title>Hello from Render</title>
+    <title>Hello from Render, in a new PR</title>
     <script src="https://cdn.jsdelivr.net/npm/canvas-confetti@1.5.1/dist/confetti.browser.min.js"></script>
     <script>
       setTimeout(() => {
